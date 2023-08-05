28bfc7ed575590e110829c8b0b3f094392d65295
Leo Mendoza <77596@sistemas.frc.utn.edu.ar>
2023-08-05 12:15:28 -0300

    mejoras

---
+++ tp1/README.md
@@ -0,0 +1,59 @@
+# Nombre del Proyecto
+
+Calculadora
+
+# Calculadora Simple
+
+La Calculadora Simple permite realizar operaciones básicas (suma, resta, multiplicación, división) entre dos números, los cuales se ingresan en las casillas correspondientes.
+
+## Funcionalidades
+
+- **Operaciones básicas:** Suma, resta, multiplicación y división entre dos números.
+
+- **Interfaz amigable:** Dos casillas de entrada para ingresar los números y un selector para elegir la operación deseada.
+
+- **Botón de cálculo:** Al hacer clic en el botón "Calcular", se muestra el resultado de la operación.
+
+- **Botón de limpieza:** Con el botón "Borrar", puedes eliminar los números ingresados para realizar una nueva operación.
+
+## Instrucciones de Uso
+
+1. Ingresa un número en la primera casilla.
+
+2. Selecciona la operación deseada en el selector.
+
+3. Ingresa el segundo número en la segunda casilla.
+
+4. Haz clic en el botón "Calcular" para obtener el resultado.
+
+5. Si deseas realizar una nueva operación, utiliza el botón "Borrar" para limpiar las casillas de entrada.
+
+## Limitaciones
+1. La calculadora admite solo números reales (números con decimales) para las operaciones.
+
+2. La precisión de los resultados está limitada por el tipo de dato de punto flotante utilizado en JavaScript.
+
+3. No es capaz de manejar operaciones con números complejos o expresiones matemáticas complejas.
+
+## Ejemplos de Cálculos
+1. Suma:
+    Número 1: 5
+    Número 2: 3
+    Resultado: 5 + 3 = 8
+
+2. Resta:
+    Número 1: 10
+    Número 2: 7
+    Resultado: 10 - 7 = 3
+
+3. Multiplicación:
+    Número 1: 2.5
+    Número 2: 4
+    Resultado: 2.5 * 4 = 10
+
+4. División:
+    Número 1: 15
+    Número 2: 3
+    Resultado: 15 / 3 = 5
+
+Con esta adición, los usuarios tendrán una visión más clara de las limitaciones de la calculadora y también encontrarán ejemplos prácticos de cómo utilizarla para realizar diferentes cálculos. Siempre es útil proporcionar información adicional para que los usuarios puedan entender mejor el alcance y las capacidades de la herramienta que están utilizando.
--- tp1/script.js
+++ tp1/script.js
@@ -1,25 +1,25 @@
 function calculate() {
-  const num1 = parseFloat(document.getElementById('num1').value);
-  const num2 = parseFloat(document.getElementById('num2').value);
+  const num1 = parseFloat(document.getElementById('num1').value); /* le asignamos a la variable num1 el valor del input segun su id*/
+  const num2 = parseFloat(document.getElementById('num2').value); /* le asignamos a la variable num2 el valor del input segun su id*/
   const operator = document.getElementById('operador').value;
-  let result;
+  let resultado;
 
   switch (operator) {
     case 'suma':
-      result = num1 + num2;
+      resultado = num1 + num2;
       break;
     case 'resta':
-      result = num1 - num2;
+      resultado = num1 - num2;
       break;
     case 'multiplicacion':
-      result = num1 * num2;
+      resultado = num1 * num2;
       break;
     case 'division':
-      if (num2 === 0) {
+      if (num2 === 0) {  /* en este 'if' verificamos si el segundo numerando es '0' para poder informar que las divisiones por 0 no esta permitidas */
         document.getElementById('resultado').innerText = "Error: No es posible dividir por cero.";
         return;
       }
-      result = num1 / num2;
+      resultado = num1 / num2;
       break;
     default:
       document.getElementById('resultado').innerText = "Operación no válida";
@@ -27,17 +27,18 @@ function calculate() {
   }
 
   // Verificar si el resultado está fuera del rango permitido
-  const maxResultValue = 1e10; // Definir el valor máximo permitido
-  const minResultValue = -1e10; // Definir el valor mínimo permitido
+  const maximoValor = 1e10; // Definir el valor máximo permitido, le puse un valor no tan alto para poder verificar si funciona
+  const minimoValor = -1e10; // Definir el valor mínimo permitido
 
-  if (result > maxResultValue || result < minResultValue) {
+  if (resultado > maximoValor || resultado < minimoValor) {
     document.getElementById('resultado').innerText = "Error: El resultado es demasiado grande o pequeño para ser mostrado.";
     return;
   }
 
-  document.getElementById('resultado').innerText = `El resultado es: ${result}`;
+  document.getElementById('resultado').innerText = `El resultado es: ${resultado}`;
 }
 
+/* esta funcion borra el contenido del los inputs y el valor en la etiquieta con el id='resultado' */
 function borrar() {
   document.getElementById('num1').value = '';
   document.getElementById('num2').value = '';
